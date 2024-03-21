a292454fa4d7e86cc7e20a9546fa8550617ac39f
lucasA49 <lucasaksu@gmail.com>
2024-03-21 12:22:47 +0100

    image restaurant ok

--- scripts/restaurants.js
+++ scripts/restaurants.js
@@ -190,7 +190,7 @@ export const restaurantDataBase = [
         shortAddress: '30 Rue Fouré',
         tel: '0251723181',
         linkToMaps: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2710.223802765004!2d-1.5495078242048335!3d47.21220307115638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805eff0bc571423%3A0x8bc6e8e73d07dae7!2sLa%20Mangouste!5e0!3m2!1sfr!2sfr!4v1710970160455!5m2!1sfr!2sfr",
-        image: './assets/Restaurants/la-mangouste.png',
+        image: './assets/Restaurants/magnouste3.svg',
         description: 'Cuisine délicate et savoureuse, accompagnée de vins sélectionnés avec passion et de cocktails parfumés.'
     }
 
